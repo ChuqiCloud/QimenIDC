c61854705f629818b7dc847e5834f66052b16865
YunQi Lv <434658198@qq.com>
2023-07-15 14:17:23 +0000

    Revert ":tada:feat(all): 首次提交"
    This reverts commit 269eab8aa262a7a59fa8dcb84b87119689321013.

--- SQL/sysuser.sql
+++
@@ -1,1 +0,0 @@
-insert into MY_TABLE (id, username, password, name, phone, email, logindate) values (1, 'mryunqi', '89cc14f2b72690dd9a7b0be672accdf9', '夜空', '18739537159', '434658198@qq.com', '1688228894211');
--- src/main/java/com/chuqiyun/proxmoxveams/controller/master/MasterBasicController.java
+++
@@ -1,9 +0,0 @@
-package com.chuqiyun.proxmoxveams.controller.master;
-
-/**
- * @author mryunqi
- * @date 2023/6/10
- */
-
-public class MasterBasicController {
-}
