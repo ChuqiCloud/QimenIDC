a523d45e2ff9687646cc5ba09459e19e7fb4ef80
mryunqi <434658198@qq.com>
2023-07-16 11:32:36 +0800

    style(utils): 优化缩进

--- src/main/java/com/chuqiyun/proxmoxveams/utils/JWTUtil.java
+++ src/main/java/com/chuqiyun/proxmoxveams/utils/JWTUtil.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -14,6 +15,7 @@ import java.util.Date;
  * @author mryunqi
  * @date 2023/4/14
  */
+@Slf4j
 public class JWTUtil {
 
     // 过期时间120分钟
