000d16c643055a5f5cc47ddcbc8b5c92f9e91936
mryunqi <434658198@qq.com>
2023-07-22 22:27:12 +0800

    feature(controller): 新增查询所有节点接口

--- src/main/java/com/chuqiyun/proxmoxveams/controller/admin/SysNodeMasterController.java
+++ src/main/java/com/chuqiyun/proxmoxveams/controller/admin/SysNodeMasterController.java
@@ -1,5 +1,6 @@
 package com.chuqiyun.proxmoxveams.controller.admin;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chuqiyun.proxmoxveams.annotation.AdminApiCheck;
 import com.chuqiyun.proxmoxveams.entity.Master;
 import com.chuqiyun.proxmoxveams.service.MasterService;
@@ -7,10 +8,7 @@ import com.chuqiyun.proxmoxveams.utils.EncryptUtil;
 import com.chuqiyun.proxmoxveams.utils.ResponseResult;
 import com.chuqiyun.proxmoxveams.utils.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -39,5 +37,30 @@ public class SysNodeMasterController {
         } else {
             return ResponseResult.fail("添加失败！");
         }
+    }
+
+    /**
+    * @Author: mryunqi
+    * @Description: 分页获取集群节点信息
+    * @DateTime: 2023/7/22 22:10
+    */
+    @AdminApiCheck
+    @GetMapping("/{adminPath}/selectNodeByPage")
+    public ResponseResult<Object> selectNodeByPage(@PathVariable("adminPath") String adminPath,
+                                                   @RequestParam(name = "page",defaultValue = "1") Integer page,
+                                                   @RequestParam(name = "size", defaultValue = "20") Integer size) throws UnauthorizedException {
+        if (!adminPath.equals(ADMIN_PATH)){
+            //判断后台路径是否正确
+            return ResponseResult.fail(ResponseResult.RespCode.NOT_PERMISSION);
+        }
+        Page<Master> masterPage = masterService.getMasterList(page,size);
+        // 将每个master的csrfToken与ticket加***处理
+        for (Master master : masterPage.getRecords()) {
+            master.setPassword("**********");
+            master.setCsrfToken("**********");
+            master.setTicket("**********");
+            master.setSshPassword("**********");
+        }
+        return ResponseResult.ok(masterPage);
     }
 }
