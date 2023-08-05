fef7236d37f47d953e876860722e18fb607fda8f
mryunqi <434658198@qq.com>
2023-08-05 15:06:01 +0800

    feature: 支持跨域

--- src/main/java/com/chuqiyun/proxmoxveams/config/WebInterceptorConfiguration.java
+++ src/main/java/com/chuqiyun/proxmoxveams/config/WebInterceptorConfiguration.java
@@ -4,6 +4,7 @@ import com.chuqiyun.proxmoxveams.interceptor.LicenseCheckInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,6 +21,26 @@ public class WebInterceptorConfiguration implements WebMvcConfigurer {
     @Bean
     public LicenseCheckInterceptor requestHandlerInterceptor(){
         return new LicenseCheckInterceptor();
+    }
+
+    /**
+     * 跨域配置
+     * @param registry registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 允许跨域访问的路径
+        registry.addMapping("/**")
+                // 允许跨域访问的源
+                .allowedOriginPatterns("*")
+                // 允许请求方法
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                // 预检间隔时间
+                .maxAge(168000)
+                // 允许头部设置
+                .allowedHeaders("*")
+                // 是否发送cookie
+                .allowCredentials(true);
     }
 
     @Override
