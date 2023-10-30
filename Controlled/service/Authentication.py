88b05bcb61a44b87c0c044ebac2cdf83b9ddc758
mryunqi <434658198@qq.com>
2023-10-30 17:33:38 +0800

    fix: 修复已知bug

--- Controlled/service/Authentication.py
+++ Controlled/service/Authentication.py
@@ -6,11 +6,11 @@ Get token value
 '''
 def get_token():
     # 判断是否存在token.key文件，否则创建
-    if not os.path.exists(os.path.join('/home/software/Controller','token.key')):
-        with open(os.path.join('/home/software/Controller','token.key'),'w') as f:
+    if not os.path.exists(os.path.join('/home/software/QAgent','token.key')):
+        with open(os.path.join('/home/software/QAgent','token.key'),'w') as f:
             f.write('token')
     # 读取token.key文件中的token值
-    with open(os.path.join('/home/software/Controller','token.key'),'r') as f:
+    with open(os.path.join('/home/software/QAgent','token.key'),'r') as f:
         token=f.read()
     # 删除结尾的换行符
     token=token.strip()
