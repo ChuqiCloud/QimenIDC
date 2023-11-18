d77b677a3950ef95a2c2aa6f819e9c0884f670aa
mryunqi <434658198@qq.com>
2023-11-18 22:14:44 +0800

    feature: 集成Prometheus 和 Grafana

--- src/main/java/com/chuqiyun/proxmoxveams/ProxmoxVeAmsApplication.java
+++ src/main/java/com/chuqiyun/proxmoxveams/ProxmoxVeAmsApplication.java
@@ -1,8 +1,12 @@
 package com.chuqiyun.proxmoxveams;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
@@ -12,6 +16,12 @@ public class ProxmoxVeAmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxmoxVeAmsApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName){
+        return (registry) -> registry.config().commonTags("application",applicationName);
     }
 
 }
