1d5f5f6c65370a043135d336a855c1575da92b76
mryunqi <434658198@qq.com>
2025-02-13 21:49:05 +0800

    feat(api): 添加虚拟机列表获取功能并优化代码
    - 新增 getVmByPage 方法，用于获取分页虚拟机列表
    -优化 getNode 方法，增加参数校验和注释
    - 在 .gitignore 中添加 install.lock 文件忽略项

--- src/main/java/com/chuqiyun/proxmoxveams/controller/api/v1/GetVmInfo.java
+++ src/main/java/com/chuqiyun/proxmoxveams/controller/api/v1/GetVmInfo.java
@@ -29,12 +29,45 @@ public class GetVmInfo {
     @Resource
     private VmInfoService vmInfoService;
 
+    /**
+     * 获取虚拟机信息
+     * 此方法通过hostId参数获取虚拟机的详细信息
+     * 主要用于处理获取虚拟机信息的请求
+     *
+     * @param hostId 虚拟机主机ID，用于标识特定的虚拟机
+     * @return 返回包含虚拟机信息的ResponseResult对象
+     *         如果hostId为0，则返回失败的ResponseResult
+     * @throws UnauthorizedException 如果用户未授权，抛出此异常
+     */
     @PublicSysApiCheck
     @GetMapping("/pve/getVmInfo")
     public ResponseResult<Object> getNode(@RequestParam(name = "hostId") Integer hostId) throws UnauthorizedException {
+        // 检查hostId是否为0，如果为0则返回错误信息
         if (hostId == 0) {
             return ResponseResult.fail("参数不能为空");
         }
+        // 调用服务层方法获取虚拟机信息，并返回成功信息
         return ResponseResult.ok(vmInfoService.getVmHostById(hostId));
     }
+
+    /**
+     * 获取虚拟机列表
+     * 此方法通过分页参数获取虚拟机的列表
+     * 主要用于处理获取虚拟机列表的请求
+     *
+     * @param page 分页参数，用于标识当前页码
+     * @param size 分页参数，用于标识每页显示的记录数
+     * @return 返回包含虚拟机列表的ResponseResult对象
+     *         如果分页参数为空，则返回失败的ResponseResult
+     * @throws UnauthorizedException 如果用户未授权，抛出此异常
+     */
+    @PublicSysApiCheck
+    @GetMapping(value = "/pve/getVmByPage")
+    public ResponseResult<Object> getVmByPage(@RequestParam(name = "page",defaultValue = "1") Integer page,
+                                              @RequestParam(name = "size",defaultValue = "20") Integer size)
+            throws UnauthorizedException{
+        HashMap<String, Object> vmByPage = vmInfoService.getVmByPage(page, size);
+        return ResponseResult.ok(vmByPage);
+    }
+
 }
