8aee5115af13429f664979909de93bd56c4cde18
mryunqi <434658198@qq.com>
2025-08-09 23:33:41 +0800

    feat(constant): 添加 IpType枚举类
    新增 IpType枚举类，用于定义不同类型的 IP 地址：
    - CLASSIC: 经典 IP
    - NAT: NAT IP
    - SPECIAL:专用 IP
    
    这些枚举值可以在系统中用于更清晰地标识和管理不同类型的 IP 地址。

---
+++ src/main/java/com/chuqiyun/proxmoxveams/constant/IpType.java
@@ -0,0 +1,16 @@
+package com.chuqiyun.proxmoxveams.constant;
+
+public class IpType {
+    /**
+     * 经典ip
+     */
+    public static final Integer CLASSIC = 0;
+    /**
+     * nat ip
+     */
+    public static final Integer NAT = 1;
+    /**
+     * 专用 ip
+     */
+    public static final Integer SPECIAL = 2;
+}
