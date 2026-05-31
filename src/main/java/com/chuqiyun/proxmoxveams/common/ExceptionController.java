7030eb83c782a15448e0f8f9d10ec02d6871332f
星禾 <954204979@qq.com>
2026-05-31 14:50:51 +0800

    修复一些显示BUG

--- src/main/java/com/chuqiyun/proxmoxveams/common/ExceptionController.java
+++ src/main/java/com/chuqiyun/proxmoxveams/common/ExceptionController.java
@@ -2,6 +2,8 @@ package com.chuqiyun.proxmoxveams.common;
 
 import com.chuqiyun.proxmoxveams.common.ResponseResult;
 import com.chuqiyun.proxmoxveams.common.exception.UnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.ClientAbortException;
 import org.apache.shiro.ShiroException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * @author mryunqi
  * @date 2023/4/16
  */
+@Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
@@ -31,11 +35,42 @@ public class ExceptionController {
         return ResponseResult.fail(ResponseResult.RespCode.UNAUTHORIZED);
     }
 
+    /**
+     * @Author: 星禾
+     * @Description: 忽略客户端主动断开连接异常
+     * @DateTime: 2026/5/29 23:43
+     */
+    @ExceptionHandler(ClientAbortException.class)
+    public void handleClientAbortException(ClientAbortException e) {
+        log.debug("客户端已断开连接: {}", e.getMessage());
+    }
+
     // 捕捉其他所有异常
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseResult<String> globalException(HttpServletRequest request, Throwable ex) {
+        if (isClientAbortException(ex)) {
+            log.debug("客户端已断开连接: {}", ex.getMessage());
+            return null;
+        }
         return new ResponseResult<>(getStatus(request).value(), ex.getMessage(), null);
+    }
+
+    private boolean isClientAbortException(Throwable ex) {
+        Throwable throwable = ex;
+        while (throwable != null) {
+            if (throwable instanceof ClientAbortException) {
+                return true;
+            }
+            if (throwable instanceof IOException && throwable.getMessage() != null) {
+                String message = throwable.getMessage().toLowerCase();
+                if (message.contains("broken pipe") || message.contains("connection reset")) {
+                    return true;
+                }
+            }
+            throwable = throwable.getCause();
+        }
+        return false;
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
