2565b06eb7c220dc903eebfb5cb9fa94a5400f76
星禾 <954204979@qq.com>
2026-07-08 10:44:11 +0800

    新增版本API，配合前端实现前后端更新提醒

--- src/main/java/com/chuqiyun/proxmoxveams/controller/api/common/VersionPublicApi.java
+++ src/main/java/com/chuqiyun/proxmoxveams/controller/api/common/VersionPublicApi.java
@@ -1,10 +1,12 @@
 package com.chuqiyun.proxmoxveams.controller.api.common;
 
+import com.alibaba.fastjson2.JSON;
 import com.chuqiyun.proxmoxveams.annotation.PublicApiCheck;
 import com.chuqiyun.proxmoxveams.common.ResponseResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -15,6 +17,8 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/api/common")
 public class VersionPublicApi {
+    private static final String NEW_VERSION_URL = "https://gitee.com/chuqicloud/soft-ware/raw/master/Cloud/version.json";
+
     @Value("${info.application.version}")
     private String version;
     /**
@@ -49,5 +53,13 @@ public class VersionPublicApi {
         map.put("version", version);
         map.put("buildVersion", buildVersion);
         return ResponseResult.ok(map);
+    }
+
+    @PublicApiCheck
+    @RequestMapping("/newversion")
+    public ResponseResult<Object> newVersion() {
+        RestTemplate restTemplate = new RestTemplate();
+        String body = restTemplate.getForObject(NEW_VERSION_URL, String.class);
+        return ResponseResult.ok(JSON.parse(body));
     }
 }
